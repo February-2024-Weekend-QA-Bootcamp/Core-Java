25e9c8b1e963c03a18f140120036175137cfad14
TofaelKabir <tofaelkabir@gmail.com>
2024-02-17 12:38:11 -0500

    Java variables Initialized

---
+++ src/lec02_01_java_variables/Car.java
@@ -0,0 +1,11 @@
+package lec02_01_java_variables;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// instead of car object, if we write other name, that is fine too
+		Car toyota = new Car();
+
+	}
+
+}
