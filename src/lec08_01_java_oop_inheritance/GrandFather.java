d2ed2a85321dea88d13d5c8855d410a299f7e2d8
TofaelKabir <tofaelkabir@gmail.com>
2024-03-31 18:00:07 -0400

    Use of super in child class and Abstraction

--- src/lec08_01_java_oop_inheritance/GrandFather.java
+++ src/lec08_01_java_oop_inheritance/GrandFather.java
@@ -1,6 +1,8 @@
 package lec08_01_java_oop_inheritance;
 
 public class GrandFather {
+	// Next time we have to create a Constructor
+	
 	// void type method
 	public void grandFatherInfo() {
 		System.out.println("Grandfather: Ismail");
